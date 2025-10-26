00e6d4d300df2e6005b050b18d4ab34f3c1f7a3f
Xaliyn <xaliyn623@gmail.com>
2025-10-27 03:26:06 +0800

    Task 1 Theme addind Photon

---
+++ src/main/java/hu/nje/townsapp/TownsappApplication.java
@@ -0,0 +1,13 @@
+package hu.nje.townsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TownsappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/nje/townsapp/TownsappApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nje.townsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
